f30101e1149bad2548445343ed6f9428bfb3efe2
agossacu <agostinaacu28@gmail.com>
2023-09-04 16:41:30 -0300

    Actualizar post

---
+++ actualizarPost.sql
@@ -0,0 +1,10 @@
+UPDATE post
+SET title = &quot;taylor swift&quot;, body = &quot;our song&quot;
+WHERE title like &quot;red%&quot;
+
+UPDATE post
+SET title = &quot;folklore&quot;, body = &quot;august&quot;
+WHERE author_id = 3 AND body like &quot;nothing new&quot;;
+UPDATE post
+SET title = &quot;Midnights&quot;, body = &quot;high infidelity&quot;
+WHERE author_id = 4 AND body like &quot;august&quot;;
