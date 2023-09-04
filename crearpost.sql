5ed5c0f7f508c271af672930a2298c4d1dc879d5
agossacu <agostinaacu28@gmail.com>
2023-09-04 16:43:06 -0300

    crear post

---
+++ crearpost.sql
@@ -0,0 +1,40 @@
+INSERT INTO post(author_id, title, body)
+VALUES(6, &quot;reputation&quot;, &quot;this is why we cant have nice things&quot;),
+(6, &quot;reputation&quot;, &quot;new years day&quot;),
+(6, &quot;reputation&quot;, &quot;end game&quot;),
+(6, &quot;reputation&quot;, &quot;so it goes&quot;),
+(6, &quot;reputation&quot;, &quot;...ready for it?&quot;),
+INSERT INTO post(author_id, title,body)
+VALUES(7, &quot;lover&quot;, &quot;lover&quot;),
+(7, &quot;lover&quot;, &quot;ME!&quot;),
+(7, &quot;lover&quot;, &quot;death by a thousand cuts&quot;),
+(7, &quot;lover&quot;, &quot;cornelia street&quot;),
+(7, &quot;lover&quot;, &quot;you need to calm down&quot;);
+INSERT INTO post(author_id, title,body)
+VALUES(8, &quot;folklore&quot;, &quot;august&quot;),
+(8, &quot;folklore&quot;, &quot;betty&quot;),
+(8, &quot;folklore&quot;, &quot;cardigan&quot;),
+(8, &quot;folklore&quot;, &quot;the one&quot;),
+(8, &quot;folklore&quot;, &quot;illicit affairs&quot;);
+INSERT INTO post(author_id, title,body)
+VALUES(9, &quot;evermore&quot;, &quot;dorothea&quot;),
+(9, &quot;evermore&quot;, &quot;right where you left me&quot;),
+(9, &quot;evermore&quot;, &quot;tis the damn season&quot;),
+(9, &quot;evermore&quot;, &quot;champagne problems&quot;),
+(9, &quot;evermore&quot;, &quot;willow&quot;),
+INSERT INTO post(author_id, title,body)
+VALUES(10, &quot;fearless&quot;, &quot;fearless&quot;),
+(10, &quot;fearless&quot;, &quot;the way i loved you&quot;),
+(10, &quot;fearless&quot;, &quot;forever &amp; always&quot;),
+(10, &quot;fearless&quot;, &quot;you belong with me&quot;),
+(10, &quot;fearless&quot;, &quot;love story&quot;);
+INSERT INTO post(author_id, title,body)
+VALUES(11, &quot;midnights&quot;, &quot;maroon&quot;),
+(11, &quot;midnights&quot;, &quot;question?&quot;),
+(11, &quot;midnights&quot;, &quot;would've, could've, should've&quot;),
+(11, &quot;midnights&quot;, &quot;anti-hero&quot;),
+(11, &quot;midnights&quot;, &quot;snow on the beach&quot;);
+/*INSERT INTO post(author_id, title,body)
+VALUES(5, &quot;1989 Taylor Version&quot;, &quot;Shake it off&quot;),
+(5, &quot;Speak now taylor Version&quot;, &quot;Speak now&quot;),
+(5, &quot;Red Taylor Version&quot;, &quot;nothing new&quot;);
