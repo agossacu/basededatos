e886952a8eaffbde4685ed78cc9d7e3113ef083a
agossacu <agostinaacu28@gmail.com>
2023-09-04 16:46:07 -0300

    crear usuarios

---
+++ crearUsuarios.sql
@@ -0,0 +1,6 @@
+INSERT into user(username, password)
+VALUES(&quot;tom&quot;, &quot;as&quot;),
+	(&quot;susana&quot;, &quot;borra&quot;),
+	(&quot;martina&quot;, &quot;chau23&quot;),
+	(&quot;nicolas&quot;, &quot;soytarado&quot;),
+	(&quot;sandra&quot;, &quot;hola&quot;);
