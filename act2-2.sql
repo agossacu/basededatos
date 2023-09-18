a05e75d651fd357b86327d392746e8a0498e7970
agossacu <agostinaacu28@gmail.com>
2023-09-18 16:10:39 -0300

    final de la actividad 2.2 modificacion

--- act2-2.sql
+++ act2-2.sql
@@ -1,2 +1,4 @@
 INSERT into address (address, city_id)
 VALUES("1328 Cornelia Street",(SELECT city_id from city WHERE city like "Rosario"))
+INSERT into store (address_id)
+SELECT address_id FROM address WHERE address like "%cornelia%";
