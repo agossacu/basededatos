b9482abbe55ace83db6de3568139b1e4a17d9b8b
agossacu <agostinaacu28@gmail.com>
2023-09-18 16:59:55 -0300

    actividad 3.1(real, la anterior era la actividad 3.3) modificacion

--- act3-1.sql
+++ act3-1.sql
@@ -1,5 +1,4 @@
 UPDATE film 
-SET language_id = (SELECT language_id FROM language WHERE name like "japo%")
+SET rental_duration = 1
 WHERE film_id in (SELECT film_id FROM film_category fc JOIN category c 
-ON c.category_id = fc.category_id WHERE name like "animation");
-	
+ON c.category_id = fc.category_id WHERE name like "action");
